e1ef02898c4bcfb9aaf67cf4f9187d698e5dd0ed
Randy <randy78520@gmail.com>
2025-06-27 10:15:08 +0200

    error45

--- lib/screens/add_plant_screen.dart
+++ lib/screens/add_plant_screen.dart
@@ -9,7 +9,6 @@ import 'package:image_picker/image_picker.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:http/http.dart' as http;
 import 'package:smartplant_app/screens/home_screen.dart';
-import 'dashboard_screen.dart';
 
 class AddPlantScreen extends StatefulWidget {
   const AddPlantScreen({super.key});
